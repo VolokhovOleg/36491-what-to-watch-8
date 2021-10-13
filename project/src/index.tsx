0de5429cf596780b2c5a3d01d7a760f72a3e0676
Oleg Volokhov <volokhov.com@gmail.com>
2021-10-13 04:06:03 +0300

    module2-task2

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const title = 'The Grand Budapest Hotel';
+const genre = 'Drama';
+const release = '2012';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      title={title}
+      genre={genre}
+      release={release}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
